d7460fb030042149af4fb1ed0ae532fbe11f3463
Keerthanabas <bkeerthanaeee@gmail.com>
2023-10-31 11:40:53 +0530

    31.10.23

--- forloop.js
+++ forloop.js
@@ -1,3 +1,21 @@
 for(number=1;number<=10;number=number+1){
     console.log(number)
 }
+
+for (number=1;number<=10;number=number+2){
+    console.log(number)
+}
+
+for(number=10;number>=1;number=number-1){
+    console.log(number)
+}
+
+for(number=1;number<=10;number=number+1){
+    if(number%2==0){
+        console.log(number)
+    }
+}
+
+for(num=1;num<=5;num=num+1){
+    console.log(number+"x2=num")
+}
